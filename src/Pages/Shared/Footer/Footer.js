b7a9e4de5da4ab18235c5cc2b59c9e58d9cc31d1
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-04-18 18:26:23 +0600

    Added footer in the projects and footer others info

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,9 +3,45 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='text-center mt-5'>
-            <p><small>copyright @ ......?!!!</small></p>
-        </div>
+        <div class="footer-dark mt-5">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="">Logo design</a></li>
+                            <li><a href="">Web design</a></li>
+                            <li><a href="">Stationery Design</a></li>
+                            <li><a href="">Packaging Design</a></li>
+                            <li><a href="">Brand identity</a></li>
+                            <li><a href="">Printable items Design</a></li>
+                            <li><a href="">oil painting</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a>Company</a></li>
+                            <li><a href="">Team</a></li>
+                            <li><a href="">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                    </div>
+                    <div class="col item social">
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                        <a href=""></a>
+                        </div>
+                </div>
+                <p class="copyright">©copyright all credit 2022 naimurrahaman</p>
+            </div>
+        </footer>
+    </div>
     );
 };
 
